214baecf694f5c4224dd7c9c4211d473d3dc57eb
Cam Brown <brownc2@carleton.edu>
2020-04-24 19:31:25 +0000

    update hw7

--- hw7.py
+++ hw7.py
@@ -1,18 +1,12 @@
 # Cam Brown
 
-
-
-
 # This module provides classical simulations of basic quantum algorithms (once you write them).
 
 # In numpy, I think that the default complex dtype varies from platform to platform. If you want to explicitly use the default type in your code, use one.dtype (where one is defined just below).
 
 import random
 import math
-import cmath
 import numpy
-
-
 
 ### CONSTANTS ###
 
@@ -26,46 +20,37 @@ ketPlus = numpy.array([1 / math.sqrt(2), 1 / math.sqrt(2)])
 ketMinus = numpy.array([1 / math.sqrt(2), -1 / math.sqrt(2)])
 
 # Our favorite one-qbit gates.
-i = numpy.array([
-    [1 + 0j, 0 + 0j],
-    [0 + 0j, 1 + 0j]])
-x = numpy.array([
-    [0 + 0j, 1 + 0j],
-    [1 + 0j, 0 + 0j]])
-y = numpy.array([
-    [0 + 0j, 0 - 1j],
-    [0 + 1j, 0 + 0j]])
-z = numpy.array([
-    [1 + 0j, 0 + 0j],
-    [0 + 0j, -1 + 0j]])
-h = numpy.array([
-    [1 / math.sqrt(2) + 0j, 1 / math.sqrt(2) + 0j],
-    [1 / math.sqrt(2) + 0j, -1 / math.sqrt(2) + 0j]])
+i = numpy.array([[1 + 0j, 0 + 0j], [0 + 0j, 1 + 0j]])
+x = numpy.array([[0 + 0j, 1 + 0j], [1 + 0j, 0 + 0j]])
+y = numpy.array([[0 + 0j, 0 - 1j], [0 + 1j, 0 + 0j]])
+z = numpy.array([[1 + 0j, 0 + 0j], [0 + 0j, -1 + 0j]])
+h = numpy.array([[1 / math.sqrt(2) + 0j, 1 / math.sqrt(2) + 0j],
+                 [1 / math.sqrt(2) + 0j, -1 / math.sqrt(2) + 0j]])
 
 # Our favorite two-qbit gates.
-cnot = numpy.array([
-    [1 + 0j, 0 + 0j, 0 + 0j, 0 + 0j], 
-    [0 + 0j, 1 + 0j, 0 + 0j, 0 + 0j],
-    [0 + 0j, 0 + 0j, 0 + 0j, 1 + 0j],
-    [0 + 0j, 0 + 0j, 1 + 0j, 0 + 0j]]) 
-swap = numpy.array([
-    [1 + 0j, 0 + 0j, 0 + 0j, 0 + 0j], 
-    [0 + 0j, 0 + 0j, 1 + 0j, 0 + 0j],
-    [0 + 0j, 1 + 0j, 0 + 0j, 0 + 0j],
-    [0 + 0j, 0 + 0j, 0 + 0j, 1 + 0j]])
+cnot = numpy.array([[1 + 0j, 0 + 0j, 0 + 0j, 0 + 0j],
+                    [0 + 0j, 1 + 0j, 0 + 0j, 0 + 0j],
+                    [0 + 0j, 0 + 0j, 0 + 0j, 1 + 0j],
+                    [0 + 0j, 0 + 0j, 1 + 0j, 0 + 0j]])
+swap = numpy.array([[1 + 0j, 0 + 0j, 0 + 0j, 0 + 0j],
+                    [0 + 0j, 0 + 0j, 1 + 0j, 0 + 0j],
+                    [0 + 0j, 1 + 0j, 0 + 0j, 0 + 0j],
+                    [0 + 0j, 0 + 0j, 0 + 0j, 1 + 0j]])
 
 ### BIT STRINGS ###
 
 # We represent an n-bit string --- that is, an element of {0, 1}^n --- in Python as a tuple of 0s and 1s.
 
+
 def string(n, m):
     '''Converts a non-negative Python integer m to its corresponding bit string. As necessary, pads with leading 0s to bring the number of bits up to n.'''
     s = ()
     while m >= 1:
-        s = (m % 2,) + s
+        s = (m % 2, ) + s
         m = m // 2
-    s = (n - len(s)) * (0,) + s
+    s = (n - len(s)) * (0, ) + s
     return s
+
 
 def integer(s):
     '''Converts a bit string s to its corresponding non-negative Python integer.'''
@@ -73,39 +58,69 @@ def integer(s):
     for k in range(len(s)):
         m = 2 * m + s[k]
     return m
-    
+
+
 # returns the norm of a complex number
 def norm(c):
-  return math.sqrt(math.pow(c.real, 2) + math.pow(c.imag, 2))
+    return math.sqrt(math.pow(c.real, 2) + math.pow(c.imag, 2))
+
+
+def application(U, kPsi):
+  return kPsi.dot(U)
+
 
 # returns the tensor product of two one-qbit states
 def tensor(ketChi, ketPsi):
-  print(ketPsi[0])
-  print(ketPsi[1])
-  tense = numpy.array(
-    [ketChi[0] * ketPsi[0],
-    ketChi[0] * ketPsi[1],
-    ketChi[1] * ketPsi[0],
-    ketChi[1] * ketPsi[1]])
-  return tense
+    # print(ketPsi[0])
+    # print(ketPsi[-1])
+    if(numpy.shape(ketChi) == (2,2)):
+      tense = numpy.array([
+        [ketChi[0][0]*ketPsi[0][0],ketChi[0][0]*ketPsi[0][1],ketChi[0][1]*ketPsi[0][0],ketChi[0][1]*ketPsi[0][1]],
+        [ketChi[0][0]*ketPsi[1][0],ketChi[0][0]*ketPsi[1][1],ketChi[0][1]*ketPsi[1][0],ketChi[0][1]*ketPsi[1][1]],
+        [ketChi[1][0]*ketPsi[0][0],ketChi[1][0]*ketPsi[0][1],ketChi[1][1]*ketPsi[0][0],ketChi[1][1]*ketPsi[0][1]],
+        [ketChi[1][0]*ketPsi[1][0],ketChi[1][0]*ketPsi[1][1],ketChi[1][1]*ketPsi[1][0],ketChi[1][1]*ketPsi[1][1]]
+      ])
+    else:
+      tense = numpy.array([
+          ketChi[0] * ketPsi[0], ketChi[0] * ketPsi[-1], ketChi[-1] * ketPsi[0],
+          ketChi[-1] * ketPsi[-1]
+      ])
+    return tense
+
 
 def first(s):
-  ## |psi> = sigma*|0> tensor'd |chi> + tau*|1> tensor'd |phi>
-  print(s)
-  x = math.sqrt(math.pow(norm(s[0]), 2) + math.pow(norm(s[1]), 2))
-  y = math.sqrt(math.pow(norm(s[2]), 2) + math.pow(norm(s[3]), 2))
-    
-  # ketPsi = tensor(x*ket0, ketChi) + tensor(y*ket1, ketPhi)
+    ## |psi> = sigma*|0> tensor'd |chi> + tau*|1> tensor'd |phi>
+    x = math.sqrt(math.pow(norm(s[0]), 2) + math.pow(norm(s[1]), 2))
+    y = math.sqrt(math.pow(norm(s[2]), 2) + math.pow(norm(s[3]), 2))
 
-  measurement = random.uniform(0, 1)
-  if(measurement < math.pow(abs(x), 2)):
-    ketChi = (1 / x) * s[:1]
-    answer = [ket0, ketChi]
-  else:
-    ketPhi = (1 / y) * s[2:]
-    answer = [ket1, ketPhi]
-  return answer
-  
+    # ketPsi = tensor(x*ket0, ketChi) + tensor(y*ket1, ketPhi)
+
+    measurement = random.uniform(0, 1)
+    if (measurement < math.pow(abs(x), 2)):
+        ketChi = (1 / x) * s[:1]
+        answer = [ket0, ketChi]
+    else:
+        ketPhi = (1 / y) * s[2:]
+        answer = [ket1, ketPhi]
+    return answer
+
+
+def last(s):
+    ## |psi> = sigma*|0> tensor'd |chi> + tau*|1> tensor'd |phi>
+    x = math.sqrt(math.pow(norm(s[0]), 2) + math.pow(norm(s[1]), 2))
+    y = math.sqrt(math.pow(norm(s[2]), 2) + math.pow(norm(s[3]), 2))
+
+    # ketPsi = tensor(x*ket0, ketChi) + tensor(y*ket1, ketPhi)
+
+    measurement = random.uniform(0, 1)
+    if (measurement < math.pow(abs(x), 2)):
+        ketChi = (1 / x) * s[:1]
+        answer = [ketChi, ket0]
+    else:
+        ketPhi = (1 / y) * s[2:]
+        answer = [ketPhi, ket1]
+    return answer
+
 
 def next(s):
     '''Given an n-bit string s, returns the next n-bit string. The order is lexicographic, except that there is a string after 1...1, namely 0...0.'''
@@ -113,23 +128,37 @@ def next(s):
     while k >= 0 and s[k] == 1:
         k -= 1
     if k < 0:
-        return len(s) * (0,)
+        return len(s) * (0, )
     else:
-        return s[:k] + (1,) + (len(s) - k - 1) * (0,)
-        
+        return s[:k] + (1, ) + (len(s) - k - 1) * (0, )
+
+
 def firstTest():
-  # Constructs an unentangled two-qbit state |0> |psi> or |1> |psi>,
-  # measures the first qbit, and then reconstructs the state.
-  print("One should see 0s.")
-  psi = uniform(1)
-  state = tensor(ket0, psi)
-  print(state)
-  meas = first(state)
-  print(state - tensor(meas[0], meas[1]))
-  psi = uniform(1)
-  state = tensor(ket1, psi)
-  meas = first(state)
-  print(state - tensor(meas[0], meas[1]))
+    # Constructs an unentangled two-qbit state |0> |psi> or |1> |psi>,
+    # measures the first qbit, and then reconstructs the state.
+    print("One should see 0s.")
+    psi = uniform(1)
+    state = tensor(ket0, psi)
+    meas = first(state)
+    print(state - tensor(meas[0], meas[1]))
+    psi = uniform(1)
+    state = tensor(ket1, psi)
+    meas = first(state)
+    print(state - tensor(meas[0], meas[1]))
+
+
+def lastTest():
+    # Constructs an unentangled two-qbit state |0> |psi> or |1> |psi>,
+    # measures the first qbit, and then reconstructs the state.
+    print("One should see 0s.")
+    psi = uniform(1)
+    state = tensor(ket0, psi)
+    meas = last(state)
+    print(state - tensor(meas[0], meas[1]))
+    psi = uniform(1)
+    state = tensor(ket1, psi)
+    meas = last(state)
+    print(state - tensor(meas[0], meas[1]))
 
 
 def nextTest(n):
@@ -138,10 +167,10 @@ def nextTest(n):
     m = integer(s)
     print(m)
     s = next(string(n, m))
-    while s != n * (0,):
-      m = integer(s)
-      print(m)
-      s = next(string(n, m))
+    while s != n * (0, ):
+        m = integer(s)
+        print(m)
+        s = next(string(n, m))
 
 
 def addition(s, t):
@@ -152,6 +181,7 @@ def addition(s, t):
 def dot(s, t):
     '''Returns the mod-2 dot product of two n-bit strings s and t.'''
     return sum([s[i] * t[i] for i in range(len(s))]) % 2
+
 
 def reduction(a):
     '''A is a list of m >= 1 bit strings of equal dimension n >= 1. In other words, A is a non-empty m x n binary matrix. Returns the reduced row-echelon form of A. A itself is left unaltered.'''
@@ -189,6 +219,10 @@ def reduction(a):
 
 ### MISCELLANY ###
 
+## def application(U, ketPsi):
+##     return ketPsi.dot(U)
+
+
 def uniform(n):
     '''Assumes n >= 0. Returns a uniformly random n-qbit state.'''
     if n == 0:
@@ -206,15 +240,15 @@ def uniform(n):
         return psi / psiNorm
 
 
-
 ### MAIN ###
+
 
 # It is conventional to have a main() function. Currently it does nothing. Change it to do whatever you want (or not).
 def main():
-    firstTest()
+
+    lastTest()
+
 
 # If the user imports this file into another program, then main() does not run. But if the user runs this file directly as a program, then main() does run.
 if __name__ == "__main__":
     main()
-
- 
